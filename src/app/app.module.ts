1d05c4326eb3663fd9f7a051668bda69f02fed76
Victor Tan <fenrirwolfe@gmail.com>
2018-05-25 15:12:49 +0800

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { MaterialModule } from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { 
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
