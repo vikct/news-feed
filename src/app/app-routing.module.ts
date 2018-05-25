4a5ef2ca7083a76d077115a6fec4c8b0281341ff
Victor Tan <fenrirwolfe@gmail.com>
2018-05-25 11:39:14 +0800

    news feed create

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
